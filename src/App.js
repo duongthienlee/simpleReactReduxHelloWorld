5eed3ecb20b0590d70f667bb5ec875485d7c535a
duongthienlee <duongthienlee@gmail.com>
2018-08-02 13:24:49 +0300

    simple hello world react redux app ( not using React-redux)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import HelloWorld from "./HelloWorld";
+import reducer from "./reducers";
+import { createStore } from "redux";
+
+const initialState = { tech: "React " };
+const store = createStore(reducer, initialState);
+
+class App extends Component {
+  render() {
+    return <HelloWorld tech={store.getState().tech} />
+  }
+}
+
+export default App;
